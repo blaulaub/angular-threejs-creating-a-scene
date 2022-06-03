45ab9fc49acda1926f7140862ba283b617d89313
Christian Fuchs <kontaktierbar@gmail.com>
2022-06-03 19:34:21 +0200

    start using three.js in typescript

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { PerspectiveCamera, Scene } from 'three';
+
 
 @Component({
   selector: 'app-view',
@@ -7,6 +9,8 @@ import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angula
 })
 export class ViewComponent implements OnInit, AfterViewInit {
 
+  // not available in ngOnInit
+  // available in ngAfterViewInit
   @ViewChild('threeFrame') threeFrame!: ElementRef<HTMLDivElement>;
 
   constructor() { }
@@ -16,5 +20,11 @@ export class ViewComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.threeFrame.nativeElement.style.background = 'green';
+    this.threeFrame.nativeElement.style.height
+
+    const scene = new Scene();
+
+    const viewAspectRatio = 1.0;
+    const camera = new PerspectiveCamera(75, viewAspectRatio, 0.1, 1000);
   }
 }
