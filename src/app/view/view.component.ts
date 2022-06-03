7bb5ba616b820d26ffa08880d9398424b262d00b
Christian Fuchs <kontaktierbar@gmail.com>
2022-06-03 19:22:22 +0200

    access a child by reference

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css']
 })
-export class ViewComponent implements OnInit {
+export class ViewComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('threeFrame') threeFrame!: ElementRef<HTMLDivElement>;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.threeFrame.nativeElement.style.background = 'green';
+  }
 }
