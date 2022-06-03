41fb05f672fa48007530cdb0d83a5a97e05a7df2
Christian Fuchs <kontaktierbar@gmail.com>
2022-06-03 19:00:01 +0200

    added three and a view port

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule
